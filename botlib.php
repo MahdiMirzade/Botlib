ed25342d640f319f96ef0e487746273cd594bb86
Mahdy Mirzade <me@mahdym.ir>
2021-08-28 17:16:21 +0430

    Fix "HTTP/2 stream 0 was not closed cleanly: PROTOCOL_ERROR (err 1)"

--- botlib.php
+++ botlib.php
@@ -24,6 +24,7 @@ class botlib {
 		curl_setopt($ch,CURLOPT_URL,$url);
 		curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 		curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+		curl_setopt($ch,CURLOPT_HTTP_VERSION,CURL_HTTP_VERSION_1_1);
 		$res = curl_exec($ch);
 		if(curl_error($ch)){
 			var_dump(curl_error($ch));
